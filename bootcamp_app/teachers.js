a86e3352c1c2f12748e307987311f41c320a4e8e
Jiani Cao <jianicakes@gmail.com>
2022-02-09 20:49:00 -0800

    update to use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON students.id = student_id
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = '${process.argv[2]}'
-  ORDER BY teacher;
-`)
+const cohort = `${process.argv[2]}`;
+const values = [cohort];
+
+const queryString = `
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`;
+
+pool.query(queryString, values)
   .then((res) => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`)
